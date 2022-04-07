b1f58e99031cf8686a25d4e15b4f33bf0833f746
giahai99 <87426577+giahai99@users.noreply.github.com>
2022-04-07 11:17:55 +0700

    update url

--- src/main/java/com/example/devopsfirstprj/controller/BookController.java
+++ src/main/java/com/example/devopsfirstprj/controller/BookController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("BookManagement/")
+@RequestMapping("Book/")
 public class BookController {
     @Autowired
     private BookRepository bookRepository;
